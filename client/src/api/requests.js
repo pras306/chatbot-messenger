3e5fb7a3619588d9554b847af0a10c948781ce59
pras306 <44067654+pras306@users.noreply.github.com>
2021-09-20 23:32:30 +0530

    Modify API routes

--- client/src/api/requests.js
+++ client/src/api/requests.js
@@ -1,5 +1,7 @@
-export const CHAT_SIGNIN = "/api/users/signin";
-export const CHAT_REGISTER = "/api/users/register";
-export const CHAT_ROOMS = "/api/rooms/";
-export const CHAT_MESSAGES = "/api/messages/";
+const BASE_URL = "https://chat-bot-messenger-app.herokuapp.com/";
+
+export const CHAT_SIGNIN = `${BASE_URL}/api/users/signin`;
+export const CHAT_REGISTER = `${BASE_URL}/api/users/register`;
+export const CHAT_ROOMS = `${BASE_URL}/api/rooms/`;
+export const CHAT_MESSAGES = `${BASE_URL}/api/messages/`;
 export const CHATBOT_API = "https://chat-proxy-server.herokuapp.com/query";
